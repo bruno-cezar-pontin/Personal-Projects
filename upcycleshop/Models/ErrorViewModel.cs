cac5daa9a50d0a579e692b4e0d725576f3a7c171
Bruno Cezar P <razecbdev@gmail.com>
2022-12-07 03:41:34 -0300

    First Commit
     Site for Wife ( in Develop...)

---
+++ upcycleshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace upcycleshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ upcycleshop/wwwroot/js/jetanim.js
@@ -0,0 +1,13 @@
+﻿/* Rename this file to jetanim.js when uploading to your website! */
+function jetanim(id = "mylogo", dt = 5000) {
+    /* Created with JETA.COM Animated Logo Wizard: https://jeta.com/js-animated-logo.php */
+    window.addEventListener("load", function () {
+        var logo = document.getElementById(id), style = document.createElement("style"); style.type = "text/css";
+        style.innerHTML = "@keyframes jetakey{50%,from,to{opacity:1}25%,75%{opacity:0}}.jetanim{animation-name:jetakey;animation-duration:1s;animation-fill-mode:both;z-index:1000}";
+        document.head.appendChild(style); set();
+        function set() { logo.classList.remove("jetanim"); setTimeout(function () { logo.classList.add("jetanim"); }, 100); }
+        var move = 0; logo.addEventListener("mouseover", function () { if (move == 0) { move = 1; set(); setTimeout(function () { move = 0; }, 1500); } });
+        var time, timer; rtime();["mousedown", "mousemove", "keypress", "scroll", "touchstart"].forEach(function (n) { document.addEventListener(n, rtime, true); });
+        function rtime(g) { (g === 1) ? time += time : time = dt; clearTimeout(timer); timer = setTimeout(function () { set(); rtime(1); }, time); }
+    });
+} jetanim();
